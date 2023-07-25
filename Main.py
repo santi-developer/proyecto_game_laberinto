bb718d413a5dcda38e34ce18cc42954273200a0c
santiago sanchez <ivsasafo30@hotmail.com>
2023-07-25 18:55:56 -0500

    creacion de el archivo main y su logica

---
+++ Main.py
@@ -0,0 +1,9 @@
+
+# pidiendo el nombre por teclado
+nombre=input("ingrese su nombre")
+
+# imprimimos una linea para dejar un espacio entre el ingreso del nombre y el mensaje de bienvenida
+print(" ")
+
+#imprimiendo mensaje de bienvenida 
+print("bienvenido jugador "+ nombre +" disfruta de tu aventura en el juego ")
